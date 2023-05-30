a37f8d362e07745442e6b54d4efa5d7ae4baf9d6
Marc Salvetti <marc@sami.eco>
2023-05-30 10:41:23 +0200

    build sort selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,7 +18,7 @@ export const GameGrid = ({ gameQuery }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={3}
-        padding={10}
+        padding={2}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
